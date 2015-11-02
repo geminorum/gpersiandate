e40d019f87ca663da05b3b335a8fb357ad7ff458
Nasser Rafie <contact@geminorum.ir>
2015-11-02 12:34:16 +0330

    widgets updated to 4.4

--- includes/widgets.class.php
+++ includes/widgets.class.php
@@ -1,22 +1,16 @@
 <?php defined( 'ABSPATH' ) or die( 'Restricted access' );
 
-// almost exact copy of core 4.3-alpha-32825
-class WP_Widget_Persian_Archives extends WP_Widget {
+require_once( ABSPATH.WPINC.'/widgets/class-wp-widget-archives.php' );
+require_once( ABSPATH.WPINC.'/widgets/class-wp-widget-calendar.php' );
 
-	public function __construct() {
-		$widget_ops = array('classname' => 'widget_archive', 'description' => __( 'A monthly archive of your site&#8217;s Posts.') );
-		parent::__construct('archives', __('Archives'), $widget_ops);
-	}
+class WP_Widget_Persian_Archives extends WP_Widget_Archives {
 
-	/**
-	 * @param array $args
-	 * @param array $instance
-	 */
+	// almost exact copy of core 4.4-beta2-35486
 	public function widget( $args, $instance ) {
 		$c = ! empty( $instance['count'] ) ? '1' : '0';
 		$d = ! empty( $instance['dropdown'] ) ? '1' : '0';
 
-		/** This filter is documented in wp-includes/default-widgets.php */
+		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
 		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Archives' ) : $instance['title'], $instance, $this->id_base );
 
 		echo $args['before_widget'];
@@ -26,7 +20,7 @@ class WP_Widget_Persian_Archives extends WP_Widget {
 
 		if ( $d ) {
 			$dropdown_id = "{$this->id_base}-dropdown-{$this->number}";
-?>
+			?>
 		<label class="screen-reader-text" for="<?php echo esc_attr( $dropdown_id ); ?>"><?php echo $title; ?></label>
 		<select id="<?php echo esc_attr( $dropdown_id ); ?>" name="archive-dropdown" onchange='document.location.href=this.options[this.selectedIndex].value;'>
 			<?php
@@ -68,11 +62,9 @@ class WP_Widget_Persian_Archives extends WP_Widget {
 			<?php gPersianDateArchives::get( $dropdown_args ); ?>
 
 		</select>
-<?php
-		} else {
-?>
+		<?php } else { ?>
 		<ul>
-<?php
+		<?php
 		/**
 		 * Filter the arguments for the Archives widget.
 		 *
@@ -86,95 +78,37 @@ class WP_Widget_Persian_Archives extends WP_Widget {
 			'type'            => 'monthly',
 			'show_post_count' => $c
 		) ) );
-?>
+		?>
 		</ul>
-<?php
+		<?php
 		}
 
 		echo $args['after_widget'];
 	}
-
-	/**
-	 * @param array $new_instance
-	 * @param array $old_instance
-	 * @return array
-	 */
-	public function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$new_instance = wp_parse_args( (array) $new_instance, array( 'title' => '', 'count' => 0, 'dropdown' => '') );
-		$instance['title'] = strip_tags($new_instance['title']);
-		$instance['count'] = $new_instance['count'] ? 1 : 0;
-		$instance['dropdown'] = $new_instance['dropdown'] ? 1 : 0;
-
-		return $instance;
-	}
-
-	/**
-	 * @param array $instance
-	 */
-	public function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '', 'count' => 0, 'dropdown' => '') );
-		$title = strip_tags($instance['title']);
-		$count = $instance['count'] ? 'checked="checked"' : '';
-		$dropdown = $instance['dropdown'] ? 'checked="checked"' : '';
-?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label> <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
-		<p>
-			<input class="checkbox" type="checkbox" <?php echo $dropdown; ?> id="<?php echo $this->get_field_id('dropdown'); ?>" name="<?php echo $this->get_field_name('dropdown'); ?>" /> <label for="<?php echo $this->get_field_id('dropdown'); ?>"><?php _e('Display as dropdown'); ?></label>
-			<br/>
-			<input class="checkbox" type="checkbox" <?php echo $count; ?> id="<?php echo $this->get_field_id('count'); ?>" name="<?php echo $this->get_field_name('count'); ?>" /> <label for="<?php echo $this->get_field_id('count'); ?>"><?php _e('Show post counts'); ?></label>
-		</p>
-<?php
-	}
 }
 
-// almost exact copy of core 4.3-alpha-32825
-class WP_Widget_Persian_Calendar extends WP_Widget {
+class WP_Widget_Persian_Calendar extends WP_Widget_Calendar {
 
-	public function __construct() {
-		$widget_ops = array('classname' => 'widget_calendar', 'description' => __( 'A calendar of your site&#8217;s Posts.') );
-		parent::__construct('calendar', __('Calendar'), $widget_ops);
-	}
+	protected static $instance = 0;
 
-	/**
-	 * @param array $args
-	 * @param array $instance
-	 */
+	// almost exact copy of core 4.4-beta2-35486
 	public function widget( $args, $instance ) {
-		/** This filter is documented in wp-includes/default-widgets.php */
+		/** This filter is documented in wp-includes/widgets/class-wp-widget-pages.php */
 		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
 
 		echo $args['before_widget'];
 		if ( $title ) {
 			echo $args['before_title'] . $title . $args['after_title'];
 		}
-		echo '<div id="calendar_wrap">';
-			gPersianDateCalendar::get();
+		if ( 0 === self::$instance ) {
+			echo '<div id="calendar_wrap" class="calendar_wrap">';
+		} else {
+			echo '<div class="calendar_wrap">';
+		}
+		gPersianDateCalendar::get();
 		echo '</div>';
 		echo $args['after_widget'];
-	}
 
-	/**
-	 * @param array $new_instance
-	 * @param array $old_instance
-	 * @return array
-	 */
-	public function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = strip_tags($new_instance['title']);
-
-		return $instance;
-	}
-
-	/**
-	 * @param array $instance
-	 */
-	public function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
-		$title = strip_tags($instance['title']);
-?>
-		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
-<?php
+		self::$instance++;
 	}
 }
