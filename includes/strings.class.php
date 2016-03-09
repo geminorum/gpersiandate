bc18766c5b30fb7a8d05af2722a84a0b0ec96503
Nasser Rafie <contact@geminorum.ir>
2016-03-10 00:20:38 +0330

    fewer calls using static variables

--- includes/strings.class.php
+++ includes/strings.class.php
@@ -4,73 +4,83 @@ class gPersianDateStrings extends gPersianDateModuleCore
 {
 	// OLD: get_month()
 	// @SEE: http://www.wikiwand.com/en/Month
-	public static function month( $month, $all = FALSE, $calendar = NULL )
+	public static function month( $formatted, $all = FALSE, $calendar = NULL )
 	{
+		static $strings = array();
+
 		if ( is_null( $calendar ) )
 			$calendar = 'Jalali';
 
-		switch( $calendar ) {
+		if ( ! isset( $strings[$calendar] ) ) {
 
-			case 'Gregorian' :
+			switch( $calendar ) {
 
-				// FIXME: !!
-				$months = array(
-					'01' => __( 'January', GPERSIANDATE_TEXTDOMAIN ), // 31 days
-					'02' => __( 'February', GPERSIANDATE_TEXTDOMAIN ), // 28 days, 29 in leap years
-					'03' => __( 'March', GPERSIANDATE_TEXTDOMAIN ), // 31 days
-					'04' => __( 'April', GPERSIANDATE_TEXTDOMAIN ), // 30 days
-					'05' => __( 'May', GPERSIANDATE_TEXTDOMAIN ), // 31 days
-					'06' => __( 'June', GPERSIANDATE_TEXTDOMAIN ), // 30 days
-					'07' => __( 'July', GPERSIANDATE_TEXTDOMAIN ), // 31 days
-					'08' => __( 'August', GPERSIANDATE_TEXTDOMAIN ), // 31 days
-					'09' => __( 'September', GPERSIANDATE_TEXTDOMAIN ), // 30 days
-					'10' => __( 'October', GPERSIANDATE_TEXTDOMAIN ), // 31 days
-					'11' => __( 'November', GPERSIANDATE_TEXTDOMAIN ), // 30 days
-					'12' => __( 'December', GPERSIANDATE_TEXTDOMAIN ), // 31 days
-				);
+				case 'Gregorian' :
 
-			break;
-			case 'Hijri' :
+					// FIXME: !!
+					$strings['Gregorian'] = array(
+						'01' => __( 'January', GPERSIANDATE_TEXTDOMAIN ), // 31 days
+						'02' => __( 'February', GPERSIANDATE_TEXTDOMAIN ), // 28 days, 29 in leap years
+						'03' => __( 'March', GPERSIANDATE_TEXTDOMAIN ), // 31 days
+						'04' => __( 'April', GPERSIANDATE_TEXTDOMAIN ), // 30 days
+						'05' => __( 'May', GPERSIANDATE_TEXTDOMAIN ), // 31 days
+						'06' => __( 'June', GPERSIANDATE_TEXTDOMAIN ), // 30 days
+						'07' => __( 'July', GPERSIANDATE_TEXTDOMAIN ), // 31 days
+						'08' => __( 'August', GPERSIANDATE_TEXTDOMAIN ), // 31 days
+						'09' => __( 'September', GPERSIANDATE_TEXTDOMAIN ), // 30 days
+						'10' => __( 'October', GPERSIANDATE_TEXTDOMAIN ), // 31 days
+						'11' => __( 'November', GPERSIANDATE_TEXTDOMAIN ), // 30 days
+						'12' => __( 'December', GPERSIANDATE_TEXTDOMAIN ), // 31 days
+					);
 
-				$months = array(
-					'01' => __( 'Muharram', GPERSIANDATE_TEXTDOMAIN ), // (Restricted/sacred) محرّم
-					'02' => __( 'Safar', GPERSIANDATE_TEXTDOMAIN ), // (Empty/Yellow) صفر
-					'03' => __( 'Rabi I', GPERSIANDATE_TEXTDOMAIN ), // Rabī' al-Awwal/Rabi' I (First Spring) ربيع الأول
-					'04' => __( 'Rabi II', GPERSIANDATE_TEXTDOMAIN ), // Rabī’ ath-Thānī/Rabi` al-Aakhir/Rabi' II (Second spring or Last spring) ربيع الآخر أو ربيع الثاني
-					'05' => __( 'Jumada I', GPERSIANDATE_TEXTDOMAIN ), // Jumada al-Awwal/Jumaada I (First Freeze) جمادى الأول
-					'06' => __( 'Jumada II', GPERSIANDATE_TEXTDOMAIN ), // Jumada ath-Thānī or Jumādā al-Thānī/Jumādā II (Second Freeze or Last Freeze) جمادى الآخر أو جمادى الثاني
-					'07' => __( 'Rajab', GPERSIANDATE_TEXTDOMAIN ), // (To Respect) رجب
-					'08' => __( 'Shaaban', GPERSIANDATE_TEXTDOMAIN ), // Sha'bān (To Spread and Distribute) شعبان
-					'09' => __( 'Ramadan', GPERSIANDATE_TEXTDOMAIN ), // Ramadān (Parched Thirst) رمضان
-					'10' => __( 'Shawwal', GPERSIANDATE_TEXTDOMAIN ), // Shawwāl (To Be Light and Vigorous) شوّال
-					'11' => __( 'Dhu al-Qidah', GPERSIANDATE_TEXTDOMAIN ), // Dhu al-Qi'dah (The Master of Truce) ذو القعدة
-					'12' => __( 'Dhu al-Hijjah', GPERSIANDATE_TEXTDOMAIN ), // Dhu al-Hijjah (The Possessor of Hajj) ذو الحجة
-				);
+				break;
+				case 'Hijri' :
 
-			break;
-			default :
-			case 'Jalali' :
+					$strings['Hijri'] = array(
+						'01' => __( 'Muharram', GPERSIANDATE_TEXTDOMAIN ), // (Restricted/sacred) محرّم
+						'02' => __( 'Safar', GPERSIANDATE_TEXTDOMAIN ), // (Empty/Yellow) صفر
+						'03' => __( 'Rabi I', GPERSIANDATE_TEXTDOMAIN ), // Rabī' al-Awwal/Rabi' I (First Spring) ربيع الأول
+						'04' => __( 'Rabi II', GPERSIANDATE_TEXTDOMAIN ), // Rabī’ ath-Thānī/Rabi` al-Aakhir/Rabi' II (Second spring or Last spring) ربيع الآخر أو ربيع الثاني
+						'05' => __( 'Jumada I', GPERSIANDATE_TEXTDOMAIN ), // Jumada al-Awwal/Jumaada I (First Freeze) جمادى الأول
+						'06' => __( 'Jumada II', GPERSIANDATE_TEXTDOMAIN ), // Jumada ath-Thānī or Jumādā al-Thānī/Jumādā II (Second Freeze or Last Freeze) جمادى الآخر أو جمادى الثاني
+						'07' => __( 'Rajab', GPERSIANDATE_TEXTDOMAIN ), // (To Respect) رجب
+						'08' => __( 'Shaaban', GPERSIANDATE_TEXTDOMAIN ), // Sha'bān (To Spread and Distribute) شعبان
+						'09' => __( 'Ramadan', GPERSIANDATE_TEXTDOMAIN ), // Ramadān (Parched Thirst) رمضان
+						'10' => __( 'Shawwal', GPERSIANDATE_TEXTDOMAIN ), // Shawwāl (To Be Light and Vigorous) شوّال
+						'11' => __( 'Dhu al-Qidah', GPERSIANDATE_TEXTDOMAIN ), // Dhu al-Qi'dah (The Master of Truce) ذو القعدة
+						'12' => __( 'Dhu al-Hijjah', GPERSIANDATE_TEXTDOMAIN ), // Dhu al-Hijjah (The Possessor of Hajj) ذو الحجة
+					);
 
-				$months = array(
-					'01' => __( 'Farvardin', GPERSIANDATE_TEXTDOMAIN ), // (31 days, فروردین)
-					'02' => __( 'Ordibehesht', GPERSIANDATE_TEXTDOMAIN ), // (31 days, اردیبهشت)
-					'03' => __( 'Khordad', GPERSIANDATE_TEXTDOMAIN ), // (31 days, خرداد)
-					'04' => __( 'Tir', GPERSIANDATE_TEXTDOMAIN ), // (31 days, تیر)
-					'05' => __( 'Mordad', GPERSIANDATE_TEXTDOMAIN ), // (31 days, مرداد)
-					'06' => __( 'Shahrivar', GPERSIANDATE_TEXTDOMAIN ), // (31 days, شهریور)
-					'07' => __( 'Mehr', GPERSIANDATE_TEXTDOMAIN ), // (30 days, مهر)
-					'08' => __( 'Aban', GPERSIANDATE_TEXTDOMAIN ), // (30 days, آبان)
-					'09' => __( 'Azar', GPERSIANDATE_TEXTDOMAIN ), // (30 days, آذر)
-					'10' => __( 'Dey', GPERSIANDATE_TEXTDOMAIN ), // (30 days, دی)
-					'11' => __( 'Bahman', GPERSIANDATE_TEXTDOMAIN ), // (30 days, بهمن)
-					'12' => __( 'Esfand', GPERSIANDATE_TEXTDOMAIN ), // (29 days- 30 days in leap year, اسفند)
-				);
+				break;
+				default :
+				case 'Jalali' :
+
+					$strings['Jalali'] = array(
+						'01' => __( 'Farvardin', GPERSIANDATE_TEXTDOMAIN ), // (31 days, فروردین)
+						'02' => __( 'Ordibehesht', GPERSIANDATE_TEXTDOMAIN ), // (31 days, اردیبهشت)
+						'03' => __( 'Khordad', GPERSIANDATE_TEXTDOMAIN ), // (31 days, خرداد)
+						'04' => __( 'Tir', GPERSIANDATE_TEXTDOMAIN ), // (31 days, تیر)
+						'05' => __( 'Mordad', GPERSIANDATE_TEXTDOMAIN ), // (31 days, مرداد)
+						'06' => __( 'Shahrivar', GPERSIANDATE_TEXTDOMAIN ), // (31 days, شهریور)
+						'07' => __( 'Mehr', GPERSIANDATE_TEXTDOMAIN ), // (30 days, مهر)
+						'08' => __( 'Aban', GPERSIANDATE_TEXTDOMAIN ), // (30 days, آبان)
+						'09' => __( 'Azar', GPERSIANDATE_TEXTDOMAIN ), // (30 days, آذر)
+						'10' => __( 'Dey', GPERSIANDATE_TEXTDOMAIN ), // (30 days, دی)
+						'11' => __( 'Bahman', GPERSIANDATE_TEXTDOMAIN ), // (30 days, بهمن)
+						'12' => __( 'Esfand', GPERSIANDATE_TEXTDOMAIN ), // (29 days- 30 days in leap year, اسفند)
+					);
+			}
 		}
 
 		if ( $all )
-			return $months;
+			return $strings[$calendar][$full];
 
-		return $months[zeroise($month, 2)];
+		$key = zeroise( $formatted, 2 );
+
+		if ( isset( $strings[$calendar][$key] ) )
+			return $strings[$calendar][$key];
+
+		return $formatted;
 	}
 
 	public static function meridiemAntePost( $formatted, $all = FALSE, $calendar = NULL )
@@ -111,99 +121,109 @@ class gPersianDateStrings extends gPersianDateModuleCore
 		return $formatted;
 	}
 
-	public static function dayoftheweek( $dayoftheweek, $all = FALSE, $calendar = NULL, $initial = FALSE )
+	public static function dayoftheweek( $formatted, $all = FALSE, $calendar = NULL, $initial = FALSE )
 	{
+		static $strings = array();
+
 		if ( is_null( $calendar ) )
 			$calendar = 'Jalali';
 
-		switch( $calendar ) {
+		$full = $initial ? 'initial' : 'full';
 
-			case 'Gregorian':
+		if ( ! isset( $strings[$calendar][$full] ) ) {
 
-				// 0 (for Sunday) through 6 (for Saturday)
+			switch( $calendar ) {
 
-				if ( $initial )
-					$week = array(
-						0 => _x( 'S', 'Day of the Week Initial: Gregorian - Sunday', GPERSIANDATE_TEXTDOMAIN ),
-						1 => _x( 'M', 'Day of the Week Initial: Gregorian - Monday', GPERSIANDATE_TEXTDOMAIN ),
-						2 => _x( 'T', 'Day of the Week Initial: Gregorian - Tuesday', GPERSIANDATE_TEXTDOMAIN ),
-						3 => _x( 'W', 'Day of the Week Initial: Gregorian - Wednesday', GPERSIANDATE_TEXTDOMAIN ),
-						4 => _x( 'T', 'Day of the Week Initial: Gregorian - Thursday', GPERSIANDATE_TEXTDOMAIN ),
-						5 => _x( 'F', 'Day of the Week Initial: Gregorian - Friday', GPERSIANDATE_TEXTDOMAIN ),
-						6 => _x( 'S', 'Day of the Week Initial: Gregorian - Saturday', GPERSIANDATE_TEXTDOMAIN ),
-					);
+				case 'Gregorian':
 
-				else
-					$week = array(
-						0 => _x( 'Sunday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
-						1 => _x( 'Monday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
-						2 => _x( 'Tuesday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
-						3 => _x( 'Wednesday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
-						4 => _x( 'Thursday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
-						5 => _x( 'Friday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
-						6 => _x( 'Saturday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
-					);
+					// 0 (for Sunday) through 6 (for Saturday)
 
-			break;
-			case 'Hijri':
+					if ( $initial )
+						$strings['Gregorian']['initial'] = array(
+							0 => _x( 'S', 'Day of the Week Initial: Gregorian - Sunday', GPERSIANDATE_TEXTDOMAIN ),
+							1 => _x( 'M', 'Day of the Week Initial: Gregorian - Monday', GPERSIANDATE_TEXTDOMAIN ),
+							2 => _x( 'T', 'Day of the Week Initial: Gregorian - Tuesday', GPERSIANDATE_TEXTDOMAIN ),
+							3 => _x( 'W', 'Day of the Week Initial: Gregorian - Wednesday', GPERSIANDATE_TEXTDOMAIN ),
+							4 => _x( 'T', 'Day of the Week Initial: Gregorian - Thursday', GPERSIANDATE_TEXTDOMAIN ),
+							5 => _x( 'F', 'Day of the Week Initial: Gregorian - Friday', GPERSIANDATE_TEXTDOMAIN ),
+							6 => _x( 'S', 'Day of the Week Initial: Gregorian - Saturday', GPERSIANDATE_TEXTDOMAIN ),
+						);
 
-				// 0 (for Saturday) through 6 (for Friday)
-				// @SEE: https://en.wikipedia.org/wiki/Islamic_calendar
+					else
+						$strings['Gregorian']['full'] = array(
+							0 => _x( 'Sunday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
+							1 => _x( 'Monday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
+							2 => _x( 'Tuesday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
+							3 => _x( 'Wednesday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
+							4 => _x( 'Thursday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
+							5 => _x( 'Friday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
+							6 => _x( 'Saturday', 'Day of the Week: Gregorian', GPERSIANDATE_TEXTDOMAIN ),
+						);
 
-				if ( $initial )
-					$week = array(
-						0 => _x( 'Sa', 'Day of the Week Initial: Hijri - as-Sabt', GPERSIANDATE_TEXTDOMAIN ),
-						1 => _x( 'Ah', 'Day of the Week Initial: Hijri - al-Ahad', GPERSIANDATE_TEXTDOMAIN ),
-						2 => _x( 'It', 'Day of the Week Initial: Hijri - al-Ithnayn', GPERSIANDATE_TEXTDOMAIN ),
-						3 => _x( 'Th', 'Day of the Week Initial: Hijri - ath-Thulatha', GPERSIANDATE_TEXTDOMAIN ),
-						4 => _x( 'Ar', 'Day of the Week Initial: Hijri - al-Arbia', GPERSIANDATE_TEXTDOMAIN ),
-						5 => _x( 'Kh', 'Day of the Week Initial: Hijri - al-Khamis', GPERSIANDATE_TEXTDOMAIN ),
-						6 => _x( 'Ju', 'Day of the Week Initial: Hijri - l-Jumuah', GPERSIANDATE_TEXTDOMAIN ),
-					);
+				break;
+				case 'Hijri':
 
-				else
-					$week = array(
-						0 => _x( 'as-Sabt', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
-						1 => _x( 'al-Aḥad', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
-						2 => _x( 'al-Ithnayn', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
-						3 => _x( 'ath-Thulāthāʼ', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
-						4 => _x( 'al-Arbi‘ā’', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
-						5 => _x( 'al-Khamīs', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
-						6 => _x( 'al-Jumu‘ah', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
-					);
+					// 0 (for Saturday) through 6 (for Friday)
+					// @SEE: https://en.wikipedia.org/wiki/Islamic_calendar
 
-			default:
-			case 'Jalali':
+					if ( $initial )
+						$strings['Hijri']['initial'] = array(
+							0 => _x( 'Sa', 'Day of the Week Initial: Hijri - as-Sabt', GPERSIANDATE_TEXTDOMAIN ),
+							1 => _x( 'Ah', 'Day of the Week Initial: Hijri - al-Ahad', GPERSIANDATE_TEXTDOMAIN ),
+							2 => _x( 'It', 'Day of the Week Initial: Hijri - al-Ithnayn', GPERSIANDATE_TEXTDOMAIN ),
+							3 => _x( 'Th', 'Day of the Week Initial: Hijri - ath-Thulatha', GPERSIANDATE_TEXTDOMAIN ),
+							4 => _x( 'Ar', 'Day of the Week Initial: Hijri - al-Arbia', GPERSIANDATE_TEXTDOMAIN ),
+							5 => _x( 'Kh', 'Day of the Week Initial: Hijri - al-Khamis', GPERSIANDATE_TEXTDOMAIN ),
+							6 => _x( 'Ju', 'Day of the Week Initial: Hijri - l-Jumuah', GPERSIANDATE_TEXTDOMAIN ),
+						);
 
-				// 0 (for Saturday) through 6 (for Friday)
+					else
+						$strings['Hijri']['full'] = array(
+							0 => _x( 'as-Sabt', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
+							1 => _x( 'al-Aḥad', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
+							2 => _x( 'al-Ithnayn', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
+							3 => _x( 'ath-Thulāthāʼ', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
+							4 => _x( 'al-Arbi‘ā’', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
+							5 => _x( 'al-Khamīs', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
+							6 => _x( 'al-Jumu‘ah', 'Day of the Week: Hijri', GPERSIANDATE_TEXTDOMAIN ),
+						);
 
-				if ( $initial )
-					$week = array(
-						0 => _x( 'Sh', 'Day of the Week Initial: Jalali - Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
-						1 => _x( 'Ye', 'Day of the Week Initial: Jalali - Yek-Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
-						2 => _x( 'Do', 'Day of the Week Initial: Jalali - Do-Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
-						3 => _x( 'Se', 'Day of the Week Initial: Jalali - Seh-Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
-						4 => _x( 'Ch', 'Day of the Week Initial: Jalali - Chahar-Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
-						5 => _x( 'Pa', 'Day of the Week Initial: Jalali - Panj-Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
-						6 => _x( 'Ad', 'Day of the Week Initial: Jalali - Adineh', GPERSIANDATE_TEXTDOMAIN ),
-					);
+				default:
+				case 'Jalali':
 
-				else
-					$week = array(
-						0 => _x( 'Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
-						1 => _x( 'Yek-Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
-						2 => _x( 'Do-Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
-						3 => _x( 'Seh-Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
-						4 => _x( 'Chahar-Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
-						5 => _x( 'Panj-Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
-						6 => _x( 'Adineh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
-					);
+					// 0 (for Saturday) through 6 (for Friday)
+
+					if ( $initial )
+						$strings['Jalali']['initial'] = array(
+							0 => _x( 'Sh', 'Day of the Week Initial: Jalali - Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
+							1 => _x( 'Ye', 'Day of the Week Initial: Jalali - Yek-Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
+							2 => _x( 'Do', 'Day of the Week Initial: Jalali - Do-Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
+							3 => _x( 'Se', 'Day of the Week Initial: Jalali - Seh-Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
+							4 => _x( 'Ch', 'Day of the Week Initial: Jalali - Chahar-Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
+							5 => _x( 'Pa', 'Day of the Week Initial: Jalali - Panj-Shanbeh', GPERSIANDATE_TEXTDOMAIN ),
+							6 => _x( 'Ad', 'Day of the Week Initial: Jalali - Adineh', GPERSIANDATE_TEXTDOMAIN ),
+						);
+
+					else
+						$strings['Jalali']['full'] = array(
+							0 => _x( 'Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
+							1 => _x( 'Yek-Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
+							2 => _x( 'Do-Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
+							3 => _x( 'Seh-Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
+							4 => _x( 'Chahar-Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
+							5 => _x( 'Panj-Shanbeh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
+							6 => _x( 'Adineh', 'Day of the Week: Jalali', GPERSIANDATE_TEXTDOMAIN ),
+						);
+			}
 		}
 
 		if ( $all )
-			return $week;
+			return $strings[$calendar][$full];
 
-		return $week[$dayoftheweek];
+		if ( isset( $strings[$calendar][$full][$formatted] ) )
+			return $strings[$calendar][$full][$formatted];
+
+		return $formatted;
 	}
 
 	// FIXME: DEPRECATED
