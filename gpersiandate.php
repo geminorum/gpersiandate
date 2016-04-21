a0ec465eb535fae85e260e008f720fb6033c311f
Nasser Rafie <contact@geminorum.ir>
2016-04-21 10:48:55 +0430

    version bump / v4x branch

--- gpersiandate.php
+++ gpersiandate.php
@@ -4,10 +4,10 @@ Plugin Name: gPersianDate
 Plugin URI: https://geminorum.ir/wordpress/gpersiandate
 Description: Persian Date for WordPress.
 Author: geminorum
-Version: 3.4.1
+Version: 4.0.0
 Author URI: http://geminorum.ir/
 GitHub Plugin URI: https://github.com/geminorum/gpersiandate
-GitHub Branch: master
+GitHub Branch: v4x
 Requires WP: 4.4
 Requires PHP: 5.3
 */
@@ -30,7 +30,7 @@ Requires PHP: 5.3
 	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
 
-define( 'GPERSIANDATE_VERSION', '3.4.1' );
+define( 'GPERSIANDATE_VERSION', '4.0.0' );
 define( 'GPERSIANDATE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GPERSIANDATE_URL', plugin_dir_url( __FILE__ ) );
 define( 'GPERSIANDATE_FILE', __FILE__ );
