b79499a7b8e6020553e738ff1558e241d754530d
Nasser Rafie <contact@geminorum.ir>
2016-04-22 15:04:53 +0430

    setting up gulp

---
+++ gulpfile.js
@@ -0,0 +1,82 @@
+(function() {
+	'use strict';
+
+	var
+		gulp = require('gulp'),
+		sass = require('gulp-sass'), // https://github.com/dlmanning/gulp-sass
+		changed = require('gulp-changed'),
+		tinypng = require('gulp-tinypng'), // https://github.com/creativeaura/gulp-tinypng
+		nano = require('gulp-cssnano'), // https://github.com/ben-eb/gulp-cssnano
+		sourcemaps = require('gulp-sourcemaps'),
+		smushit = require('gulp-smushit'), // https://github.com/heldr/gulp-smushit
+		excludeGitignore = require('gulp-exclude-gitignore'), // https://github.com/sboudrias/gulp-exclude-gitignore
+		wpPot = require('gulp-wp-pot'), // https://github.com/rasmusbe/gulp-wp-pot
+		sort = require('gulp-sort'),
+		fs = require('fs');
+
+	var
+		json = JSON.parse(fs.readFileSync('./package.json'));
+
+	gulp.task('tinypng', function() {
+
+		return gulp.src('./assets/images/raw/*.png')
+
+		.pipe(tinypng(''))
+
+		.pipe(gulp.dest('./assets/images'));
+	});
+
+	gulp.task('smushit', function() {
+
+		return gulp.src('./assets/images/raw/**/*.{jpg,png}')
+
+		.pipe(smushit())
+
+		.pipe(gulp.dest('./assets/images'));
+	});
+
+	gulp.task('pot', function() {
+
+		return gulp.src(['./**/*.php', '!./assets/libs/**'])
+
+		.pipe(excludeGitignore())
+
+		.pipe(sort())
+
+		.pipe(wpPot(json._pot))
+
+		.pipe(gulp.dest('./languages'));
+	});
+
+	gulp.task('sass', function() {
+
+		return gulp.src('./assets/sass/**/*.scss')
+
+		.pipe(sourcemaps.init())
+
+		.pipe(sass().on('error', sass.logError))
+
+		.pipe(nano({
+			discardComments: {
+				removeAll: true
+			}
+		}))
+
+		.pipe(sourcemaps.write('./maps'))
+
+		.pipe(gulp.dest('./assets/css'));
+	});
+
+	gulp.task('watch', function() {
+
+		gulp.watch('./assets/sass/**/*.scss', [
+			'sass'
+		]);
+	});
+
+	gulp.task('default', function() {
+
+		console.log('Hi, I\'m Gulp!');
+	});
+
+}());
