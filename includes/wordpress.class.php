b9be2821fe8795ae7a2e4b0dead80fdcf7f04402
Nasser Rafie <contact@geminorum.ir>
2016-10-26 08:40:58 +0330

    main/wordpress: new filters

--- includes/wordpress.class.php
+++ includes/wordpress.class.php
@@ -29,7 +29,9 @@ class gPersianDateWordPress extends gPersianDateModuleCore
 		add_filter( 'comments_number', array( 'gPersianDateTranslate', 'numbers' ), 12 );
 		add_filter( 'human_time_diff', array( 'gPersianDateTranslate', 'numbers' ), 12 );
 
-		add_filter( 'pre_insert_term', array( $this, 'pre_insert_term' ), 10, 2 );
+		add_filter( 'wp_link_pages_link', array( 'gPersianDateTranslate', 'html' ), 12 );
+
+		// add_filter( 'pre_insert_term', array( $this, 'pre_insert_term' ), 10, 2 );
 		add_filter( 'pre_term_name', array( 'gPersianDateTranslate', 'numbers' ) );
 		add_filter( 'pre_term_description', array( 'gPersianDateTranslate', 'html' ) );
 
