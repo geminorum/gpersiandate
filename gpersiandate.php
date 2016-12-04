3976dea24b585ad374185b4ac3158b280c1d2677
Nasser Rafie <contact@geminorum.ir>
2016-12-04 09:21:24 +0330

    main: textdomain moved in init func

--- gpersiandate.php
+++ gpersiandate.php
@@ -19,7 +19,6 @@ define( 'GPERSIANDATE_VERSION', '3.4.1' );
 define( 'GPERSIANDATE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GPERSIANDATE_URL', plugin_dir_url( __FILE__ ) );
 define( 'GPERSIANDATE_FILE', basename( GPERSIANDATE_DIR ).'/'.basename( __FILE__ ) );
-defined( 'GPERSIANDATE_TEXTDOMAIN' ) or define( 'GPERSIANDATE_TEXTDOMAIN', 'gpersiandate' );
 
 function gpersiandate_init(){
 
@@ -55,6 +54,7 @@ function gpersiandate_init(){
 		if ( file_exists( GPERSIANDATE_DIR.'includes/'.$include.'.class.php' ) )
 			require_once( GPERSIANDATE_DIR.'includes/'.$include.'.class.php' );
 
+	defined( 'GPERSIANDATE_TEXTDOMAIN' ) or define( 'GPERSIANDATE_TEXTDOMAIN', 'gpersiandate' );
 	defined( 'GPERSIANDATE_TIMEZONE' ) or define( 'GPERSIANDATE_TIMEZONE', gPersianDateTimeZone::current() );
 	defined( 'GPERSIANDATE_LOCALE' ) or define( 'GPERSIANDATE_LOCALE', get_locale() );
 	defined( 'GPERSIANDATE_FIXNONPERSIAN' ) or define( 'GPERSIANDATE_FIXNONPERSIAN', TRUE );
