731daf4974042d921026208d06e879fcc123d027
Nasser Rafie <contact@geminorum.ir>
2016-12-04 09:24:37 +0330

    main/search: better handling arabic char/numbers

--- includes/search.class.php
+++ includes/search.class.php
@@ -7,52 +7,52 @@ class gPersianDateSearch extends gPersianDateModuleCore
 
 	protected function setup_actions()
 	{
-		add_filter( 'posts_request', array( $this, 'posts_request' ), 20 );
-		add_filter( 'posts_search', array( $this, 'posts_request' ), 20 );
+		add_filter( 'posts_search', array( $this, 'posts_search' ), 20, 2 );
 
 		add_filter( 'get_search_query', array( 'gPersianDateTranslate', 'chars' ) );
 	}
 
-	// Originally from wp-jalali
-	public function posts_request( $query )
+	// TODO: english numbers too?
+	protected static function chars()
 	{
-		if ( strstr( $query, 'LIKE' ) )
-			if ( strstr( $query, "ی" )
-				|| strstr( $query, "ک" )
-				|| strstr( $query, "ي" )
-				|| strstr( $query, "ك" )
-				|| strstr( $query, "٤" )
-				|| strstr( $query, "٥" )
-				|| strstr( $query, "٦")
-				|| strstr( $query, "۴" )
-				|| strstr( $query, "۵" )
-				|| strstr( $query, "۶" ) )
-					$query = preg_replace_callback( "/(\([^\)\(]* LIKE '([^']*)'\))/",
-						array( __CLASS__, 'posts_request_callback' ), $query );
-		return $query;
+		return array(
+			'ي' => 'ی',
+			'ك' => 'ک',
+			'٠' => '۰',
+			'١' => '۱',
+			'٢' => '۲',
+			'٣' => '۳',
+			'٤' => '۴',
+			'٥' => '۵',
+			'٦' => '۶',
+			'٧' => '۷',
+			'٨' => '۸',
+			'٩' => '۹',
+		);
 	}
 
-	// Originally from wp-jalali
-	public static function posts_request_callback( $matches )
+	public function posts_search( $search, $wp_query )
 	{
-		return
-			"( ".
-			str_replace(
-				$matches[2],
-				str_replace(
-					array( "ي", "ك", "٤", "٥", "٦" ),
-					array( "ی", "ک", "۴", "۵", "۶" ),
-					$matches[2]
-				),
-				$matches[1]
-			)." OR ".
-			str_replace(
-				$matches[2],
-				str_replace(
-					array( "ی", "ک", "۴", "۵", "۶" ),
-					array( "ي", "ك", "٤", "٥", "٦" ),
-					$matches[2]
-				), $matches[1]
-			)." )";
+		if ( ! gPersianDateText::has( $search, 'LIKE' ) )
+			return $search;
+
+		$chars = self::chars();
+
+		if ( ! gPersianDateText::has( $search, array_keys( $chars ) + $chars ) )
+			return $search;
+
+		return preg_replace_callback( "/(\([^\)\(]* LIKE '([^']*)'\))/",
+			array( __CLASS__, 'duplicateClause' ), $search );
+	}
+
+	public static function duplicateClause( $matches )
+	{
+		$chars   = self::chars();
+		$join    = gPersianDateText::has( $matches[1], 'NOT LIKE' ) ? ' AND ' : ' OR ';
+		$persian = str_replace( array_keys( $chars ), array_values( $chars ), $matches[2] );
+		$arabic  = str_replace( array_values( $chars ), array_keys( $chars ), $matches[2] );
+
+		return '('.str_replace( $matches[2], $persian, $matches[1] )
+			.$join.str_replace( $matches[2], $arabic, $matches[1] ).')';
 	}
 }
