d842cc54525ce912fbcd5894b1f55facec4ed112
Nasser Rafie <contact@geminorum.ir>
2017-10-21 07:23:41 +0330

    main/shortcodes: :new: [entry-link-published]

--- includes/shortcodes.class.php
+++ includes/shortcodes.class.php
@@ -13,6 +13,7 @@ class gPersianDateShortCodes extends gPersianDateModuleCore
 		$this->shortcodes( [
 			'date-archives-clean'   => 'shortcode_archives_clean',
 			'date-archives-compact' => 'shortcode_archives_compact',
+			'entry-link-published'  => 'shortcode_entry_published',
 		] );
 	}
 
@@ -73,5 +74,62 @@ class gPersianDateShortCodes extends gPersianDateModuleCore
 		}
 
 		return self::shortcodeWrap( $html, 'archives-compact', $args );
+	}
+
+	// @REF: http://justintadlock.com/?p=2507
+	public function shortcode_entry_published( $atts = array(), $content = NULL, $tag = '' )
+	{
+		$args = shortcode_atts( [
+			'format'  => get_option( 'date_format' ),
+			'title'   => _x( 'Archive for %s', 'Shortcodes: Entry Published Title', GPERSIANDATE_TEXTDOMAIN ),
+			'context' => NULL,
+			'wrap'    => TRUE,
+			'before'  => '',
+			'after'   => '',
+		], $atts, $tag );
+
+		if ( FALSE === $args['context'] )
+			return NULL;
+
+		$html = '';
+
+		$year  = get_the_time( 'Y' );
+		$month = get_the_time( 'm' );
+		$day   = get_the_time( 'j' );
+
+		$number_year  = gPersianDateTranslate::numbers_back( $year );
+		$number_month = gPersianDateTranslate::numbers_back( $month );
+		$number_day   = gPersianDateTranslate::numbers_back( $day );
+
+		$url_year  = gPersianDateLinks::build( 'year', $number_year );
+		$url_month = gPersianDateLinks::build( 'month', $number_year, $number_month );
+		$url_day   = gPersianDateLinks::build( 'day', $number_year, $number_month, $number_day );
+
+		$link_year  = '<a href="'.esc_url( $url_year ).'" title="'.esc_attr( sprintf( $args['title'], $year ) ).'" rel="nofollow">'.$year.'</a>';
+		$link_month = '<a href="'.esc_url( $url_month ).'" title="'.esc_attr( sprintf( $args['title'], get_the_time( 'F Y' ) ) ).'" rel="nofollow">'.get_the_time( 'F' ).'</a>';
+		$link_day   = '<a href="'.esc_url( $url_day ).'" title="'.esc_attr( sprintf( $args['title'], get_the_time( $args['format'] ) ) ).'" rel="nofollow">'.$day.'</a>';
+
+		// FIXME: problem with slashes in the format
+		// $format = is_rtl() ? strrev( $args['format'] ) : $args['format'];
+		$format = $args['format'];
+
+		for ( $i = 0; $i < strlen( $format ); $i++ ) {
+
+			if ( preg_match( '/[oYy]/', $format[$i] ) )
+				$html.= $link_year;
+
+			else if ( preg_match( '/[FmMn]/', $format[$i] ) )
+				$html.= $link_month;
+
+			else if ( preg_match( '/[dDjl]/', $format[$i] ) )
+				$html.= $link_day;
+
+			else
+				$html.= $format[$i];
+		}
+
+		// $html = '&lrm;'.$html.'&rlm;';
+
+		return self::shortcodeWrap( $html, 'link-published', $args, FALSE );
 	}
 }
