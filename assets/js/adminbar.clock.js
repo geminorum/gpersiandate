d71fbb8702fb07a9495f596c4d4addc48bf32b83
Nasser Rafie <contact@geminorum.ir>
2018-02-23 22:42:08 +0330

    assets/scripts: coding standard

--- assets/js/adminbar.clock.js
+++ assets/js/adminbar.clock.js
@@ -1,43 +1,38 @@
-(function($) {
-  'use strict';
+/* global jQuery */
 
-  var m = {}
+(function ($) {
+  var m = {};
 
   m.wrapper = '#gpd-now';
 
-  m.toPersianDigit = function(number){
+  m.toPersianDigit = function (number) {
     var pzero = '۰'.charCodeAt(0);
-    return number.toString().replace(/\d+/g,function (match) {
+    return number.toString().replace(/\d+/g, function (match) {
       return match.split('').map(function (number) {
-        return String.fromCharCode(pzero+parseInt(number))
+        return String.fromCharCode(pzero + parseInt(number));
       }).join('');
-    })
+    });
   };
 
-  m.toEnglishDigit = function(number){
-    return number.toString().replace(/[۱۲۳۴۵۶۷۸۹۰]+/g,function (match) {
+  m.toEnglishDigit = function (number) {
+    return number.toString().replace(/[۱۲۳۴۵۶۷۸۹۰]+/g, function (match) {
       return match.split('').map(function (number) {
-        return number.charCodeAt(0)%1776;
+        return number.charCodeAt(0) % 1776;
       }).join('');
-    })
+    });
   };
 
-
-  m.updateClock = function(){
-
-    var
-      wrapper = $(m.wrapper),
-      currentTime = new Date();
-
-    var
-      currentHours = currentTime.getHours(),
-      currentMinutes = currentTime.getMinutes(),
-      currentSeconds = currentTime.getSeconds();
+  m.updateClock = function () {
+    var wrapper = $(m.wrapper);
+    var currentTime = new Date();
+    var currentHours = currentTime.getHours();
+    var currentMinutes = currentTime.getMinutes();
+    var currentSeconds = currentTime.getSeconds();
 
     // pad the minutes and seconds with leading zeros, if required
-    currentHours = ( currentHours < 10 ? "0" : "" ) + currentHours;
-    currentMinutes = ( currentMinutes < 10 ? "0" : "" ) + currentMinutes;
-    currentSeconds = ( currentSeconds < 10 ? "0" : "" ) + currentSeconds;
+    currentHours = (currentHours < 10 ? '0' : '') + currentHours;
+    currentMinutes = (currentMinutes < 10 ? '0' : '') + currentMinutes;
+    currentSeconds = (currentSeconds < 10 ? '0' : '') + currentSeconds;
 
     // choose either "AM" or "PM" as appropriate
     // var timeOfDay = ( currentHours < 12 ) ? "AM" : "PM";
@@ -51,18 +46,18 @@
     // compose the string for display
     // var currentTimeString = currentHours + ":" + currentMinutes + ":" + currentSeconds + " " + timeOfDay;
     // var currentTimeString = GPDfixNumbers( currentHours + ":" + currentMinutes, GPD_clock.local );
-    var currentTimeString = currentHours + ":" + currentMinutes+ ":" + currentSeconds;
+    var currentTimeString = currentHours + ':' + currentMinutes + ':' + currentSeconds;
 
-    if ( 'fa_IR' == wrapper.data('locale') )
+    if (wrapper.data('locale') === 'fa_IR') {
       currentTimeString = m.toPersianDigit(currentTimeString);
+    }
 
     wrapper.html(currentTimeString);
     // console.log(currentTimeString);
   };
 
-  $( document ).ready( function () {
+  $(document).ready(function () {
     setInterval(m.updateClock, 1000);
     // setInterval(m.updateClock, 60*1000);
   });
-
 }(jQuery));
