e3868110a9bc9461f0a2ff28200a67822f9c0125
Nasser Rafie <contact@geminorum.ir>
2019-03-04 17:11:54 +0330

    :wrench:

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@
   var fs = require('fs');
 
   var pkg = require('./package.json');
-  var config = require('./gulpconfig.json');
+  var config = require('./gulp.config.json');
 
   var env = config.env;
   var banner = config.banner.join('\n');
