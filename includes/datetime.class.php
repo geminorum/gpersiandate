d243432905982e5e7344196e9c7df35b9d0756c9
Nasser Rafie <contact@geminorum.ir>
2019-04-14 07:23:32 +0430

    main/datetime: alternative method of calculation

--- includes/datetime.class.php
+++ includes/datetime.class.php
@@ -350,6 +350,12 @@ class gPersianDateDateTime extends gPersianDateModuleCore
 		return self::checkJalali( 12, 30, $year );
 	}
 
+	// @REF: https://jdf.scr.ir/tarikh/?t=tahvile_sal#jn3
+	public static function isLeapYearJalali_Alt( $year )
+	{
+		return ( ( ( $year % 33 ) % 4 ) - 1 ) == ( (int) ( ( $year % 33 ) * 0.05 ) );
+	}
+
 	// FIXME
 	public static function isLeapYearHijri( $year )
 	{
@@ -496,6 +502,98 @@ class gPersianDateDateTime extends gPersianDateModuleCore
 	private static function div( $a, $b )
 	{
 		return (int) ( $a / $b );
+	}
+
+	/**
+	 * Author: JDF.SCR.IR =>> Download Full Version : http://jdf.scr.ir/jdf
+	 * License: GNU/LGPL _ Open Source & Free _ Version: 2.70 : [2017=1395]
+	 *
+	 * 1461 = 365*4 + 4/4   &  146097 = 365*400 + 400/4 - 400/100 + 400/400
+	 * 12053 = 365*33 + 32/4    &    36524 = 365*100 + 100/4 - 100/100
+	*/
+	public static function toJalali_Alt( $gy, $gm, $gd )
+	{
+		$g_d_m = [ 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334 ];
+
+		if ( $gy > 1600 ){
+			$jy = 979;
+			$gy-= 1600;
+		} else {
+			$jy = 0;
+			$gy-= 621;
+		}
+
+		$gy2 = $gm > 2 ? $gy + 1 : $gy;
+
+		$days = ( 365 * $gy )
+			+ ( (int) ( ( $gy2 + 3 ) / 4 ) )
+			- ( (int) ( ( $gy2 + 99 ) / 100 ) )
+			+ ( (int) ( ( $gy2 + 399 ) / 400 ) )
+			- 80 + $gd + $g_d_m[$gm - 1];
+
+		$jy+= 33 * ( (int) ( $days / 12053 ) );
+		$days%= 12053;
+		$jy+= 4 * ( (int) ( $days / 1461 ) );
+		$days%= 1461;
+		$jy+= (int) ( ( $days - 1 ) / 365 );
+
+		if ( $days > 365 )
+			$days = ( $days - 1 ) % 365;
+
+		if ( $days < 186 ) {
+			$jm = 1 + (int) ( $days / 31 );
+			$jd = 1 + ( $days % 31 );
+		} else {
+			$jm = 7 + (int) ( ( $days - 186 ) / 30 );
+			$jd = 1 + ( ( $days - 186 ) % 30 );
+		}
+
+		return [ $jy, $jm, $jd ];
+	}
+
+	/**
+	 * Author: JDF.SCR.IR =>> Download Full Version : http://jdf.scr.ir/jdf
+	 * License: GNU/LGPL _ Open Source & Free _ Version: 2.70 : [2017=1395]
+	*/
+	public static function fromJalali_Alt( $jy, $jm, $jd )
+	{
+		if ( $jy > 979 ) {
+			$gy = 1600;
+			$jy-= 979;
+		} else {
+			$gy = 621;
+		}
+
+		$days = ( 365 * $jy ) + ( ( (int) ( $jy / 33 ) ) * 8 ) + ( (int) ( ( ( $jy % 33 ) + 3 ) / 4 ) ) + 78 + $jd + ( ( $jm < 7 ) ? ( $jm - 1 ) * 31 : ( ( $jm - 7 ) * 30 ) + 186 );
+		$gy+= 400 * ( (int) ( $days / 146097 ) );
+		$days%= 146097;
+
+		if ( $days > 36524 ) {
+			$gy+= 100 * ( (int) ( --$days / 36524 ) );
+			$days%= 36524;
+
+			if ( $days >= 365 )
+				$days++;
+		}
+
+		$gy+= 4 * ( (int) ( ( $days ) / 1461 ) );
+		$days%= 1461;
+		$gy+= (int) ( ( $days - 1) / 365 );
+
+		if ( $days > 365 )
+			$days = ( $days - 1 ) % 365;
+
+		$gd = $days + 1;
+
+		foreach ( [ 0, 31, ( ( ( $gy % 4 == 0 ) && ( $gy % 100 != 0 ) ) || ( $gy % 400 == 0 ) ) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ] as $gm => $v ) {
+
+			if ( $gd <= $v )
+				break;
+
+			$gd-= $v;
+		}
+
+		return [ $gy, $gm, $gd ];
 	}
 
 	public static function toHijri( $year, $month, $day )
