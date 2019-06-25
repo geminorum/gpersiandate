aa32003557999561011234b8392ad3dfdd50eaaf
Nasser Rafie <contact@geminorum.ir>
2019-06-25 15:18:15 +0430

    :up: min php 5.6.20

--- gpersiandate.php
+++ gpersiandate.php
@@ -12,18 +12,18 @@ Network: false
 GitHub Plugin URI: https://github.com/geminorum/gpersiandate
 GitHub Branch: master
 Requires WP: 4.4
-Requires PHP: 5.4
+Requires PHP: 5.6.20
 */
 
 define( 'GPERSIANDATE_VERSION', '3.6.1' );
-define( 'GPERSIANDATE_MIN_PHP', '5.4.0' );
+define( 'GPERSIANDATE_MIN_PHP', '5.6.20' );
 define( 'GPERSIANDATE_DIR', plugin_dir_path( __FILE__ ) );
 define( 'GPERSIANDATE_URL', plugin_dir_url( __FILE__ ) );
 define( 'GPERSIANDATE_FILE', basename( GPERSIANDATE_DIR ).'/'.basename( __FILE__ ) );
 
 defined( 'GPERSIANDATE_TEXTDOMAIN' ) or define( 'GPERSIANDATE_TEXTDOMAIN', 'gpersiandate' );
 
-if ( version_compare( GPERSIANDATE_MIN_PHP, PHP_VERSION, '>=' ) ) {
+if ( version_compare( GPERSIANDATE_MIN_PHP, phpversion(), '>=' ) ) {
 
 	if ( is_admin() ) {
 		echo '<div class="notice notice-warning notice-alt is-dismissible"><p dir="ltr">';
