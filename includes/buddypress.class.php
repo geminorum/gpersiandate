4b702b051c71f473d11241ce368d1e8fc7ec9b31
Nasser Rafie <contact@geminorum.ir>
2019-08-16 09:30:07 +0430

    main/buddypress: retry bp filters

--- includes/buddypress.class.php
+++ includes/buddypress.class.php
@@ -7,10 +7,14 @@ class gPersianDateBuddyPress extends gPersianDateModuleCore
 
 	protected function setup_actions()
 	{
-		add_filter( 'bp_get_total_group_count_for_user', [ 'gPersianDateTranslate', 'numbers' ] );
-
+		add_filter( 'bp_core_number_format', [ 'gPersianDateTranslate', 'numbers' ] );
 		add_filter( 'bp_core_time_since', [ 'gPersianDateTranslate', 'numbers' ] );
-		add_filter( 'bp_activity_get_comment_count', [ 'gPersianDateTranslate', 'numbers' ] );
-		add_filter( 'bp_activity_recurse_comment_count', [ 'gPersianDateTranslate', 'numbers' ] );
+
+		add_filter( 'bp_groups_multiple_new_membership_requests_notification', [ 'gPersianDateTranslate', 'legacy' ] );
+		add_filter( 'bp_groups_multiple_membership_request_accepted_notification', [ 'gPersianDateTranslate', 'legacy' ] );
+		add_filter( 'bp_groups_multiple_membership_request_rejected_notification', [ 'gPersianDateTranslate', 'legacy' ] );
+		add_filter( 'bp_groups_multiple_member_promoted_to_admin_notification', [ 'gPersianDateTranslate', 'legacy' ] );
+		add_filter( 'bp_groups_multiple_member_promoted_to_mod_notification', [ 'gPersianDateTranslate', 'legacy' ] );
+		add_filter( 'bp_groups_multiple_group_invite_notification', [ 'gPersianDateTranslate', 'legacy' ] );
 	}
 }
