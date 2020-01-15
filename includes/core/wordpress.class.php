381029afa4b7cdf72b49c48a9292658fb9c71999
Nasser Rafie <contact@geminorum.ir>
2020-01-15 07:06:54 +0330

    :peach:

--- includes/core/wordpress.class.php
+++ includes/core/wordpress.class.php
@@ -3,12 +3,22 @@
 class gPersianDateWP
 {
 
-	// @REF: `vars.php`
-	public static function pageNow()
+	// Checks compatibility with the current WordPress version.
+	// @REF: `is_wp_version_compatible()`
+	public static function isWPcompatible( $required )
 	{
-		if ( preg_match( '#([^/]+\.php)([?/].*?)?$#i', $_SERVER['PHP_SELF'], $matches ) )
-			return strtolower( $matches[1] );
+		return empty( $required ) || version_compare( $GLOBALS['wp_version'], $required, '>=' );
+	}
 
-		return 'index.php';
+	// @REF: `vars.php`
+	// TODO: support arrays
+	public static function pageNow( $page = NULL )
+	{
+		$now = 'index.php';
+
+		if ( preg_match( '#([^/]+\.php)([?/].*?)?$#i', $_SERVER['PHP_SELF'], $matches ) )
+			$now = strtolower( $matches[1] );
+
+		return is_null( $page ) ? $now : ( $now == $page );
 	}
 }
