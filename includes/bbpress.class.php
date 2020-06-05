246e6a45b2c82893b8efe0bb237880bd0911c628
Nasser Rafie <contact@geminorum.ir>
2020-06-05 23:58:06 +0430

    main/bbpress: avoid double convertion of numbers

--- includes/bbpress.class.php
+++ includes/bbpress.class.php
@@ -9,5 +9,12 @@ class gPersianDateBBPress extends gPersianDateModuleCore
 	{
 		add_filter( 'bbp_number_format', [ 'gPersianDateTranslate', 'numbers' ], 12 );
 		add_filter( 'bbp_get_time_since', [ 'gPersianDateTranslate', 'numbers' ], 12 );
+
+		add_filter( 'bbp_number_format_i18n', [ $this, 'number_format_i18n' ], 12, 3 );
+	}
+
+	public function number_format_i18n( $formatted, $number, $decimals )
+	{
+		return $number; // avoid double convertion
 	}
 }
